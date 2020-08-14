3a853910cf251ad78369e829f771b655cf3ea1bc
Jasmine Kwon <jhkwon1472@gmail.com>
2020-08-14 03:14:48 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -60,9 +60,7 @@ def add_characters
    #   - The first hash should have the :name key set to "Prince Escalus" 
    #   - The second hash should have the :name key set to "Apothecary" 
    # Code your solution here:
-    epic_tragedy[:montague][:additonal_characters] = 
-      [{name: "Prince Escalus"}, {name: "Apothecary"}]
-    epic_tragedy[:capulet][:additonal_characters] = 
+    epic_tragedy[:additonal_characters] = 
       [{name: "Prince Escalus"}, {name: "Apothecary"}]
 
 
